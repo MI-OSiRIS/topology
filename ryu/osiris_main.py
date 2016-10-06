0fb98f89f1720f3d46d0c9f8eb5cf9aad52abd38
Supreeth S <supreeth90@gmail.com>
2016-10-06 19:13:37 -0400

    Code to dynamically create nodes, ports and links

--- ryu/osiris_main.py
+++ ryu/osiris_main.py
@@ -18,6 +18,9 @@ from ryu.lib.packet import lldp
 from pprint import pprint
 import codecs
 import struct
+from unis.models import *
+from unis.runtime import Runtime
+
 # from lldp_host_parser import LLDPHost
 
 PATH = os.path.dirname(__file__)
@@ -31,6 +34,7 @@ class OSIRISApp(app_manager.RyuApp):
         super(OSIRISApp, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
         self.datapaths = {}
+        self.rt = Runtime("http://localhost:8888")
         
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -47,6 +51,7 @@ class OSIRISApp(app_manager.RyuApp):
                 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        print("**** switch_features_handler *****")
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -63,6 +68,7 @@ class OSIRISApp(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
+
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -77,16 +83,61 @@ class OSIRISApp(app_manager.RyuApp):
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
-        
+
+    @set_ev_cls(event.EventSwitchEnter)
+    def switch_enter_handler(self, ev):
+        print("**** switch_enter_handler *****")
+        # address = ev.switch.dp.address
+        # pprint(ev.switch.dp.__dict__)
+        # for port in ev.switch.ports:
+        #     pprint(port.to_dict())
+        # # pprint(ev.switch.ports)
+        # dpid = ev.switch.dp.id
+        # print(dpid)s
+        self.check_add_switch(ev.switch, ev.switch.dp)
+
+
+    def check_add_switch(self, switch, datapath):
+        switch_name = "switch:"+str(datapath.id)
+        print("*** PRINT 111***")
+        switch_node = self.check_node(switch_name)
+        print("*** PRINT 112***")
+        if switch_node is None:
+            print("*** PRINT 113***")
+            ports_list = []
+            port_object = None
+            switch_node = Node({"name": switch_name})
+            self.rt.insert(switch_node, commit=True)
+            print("*** PRINT 114***")
+            for port in switch.ports:
+                print("*** PRINT 115***")
+                port_object = Port({"name": port.name.decode("utf-8"), "index": str(port.port_no), "address":
+                    {"address": port.hw_addr, "type": "mac"}})
+                print("*** PRINT 116***")
+                self.rt.insert(port_object, commit=True)
+                print("*** PRINT 117***")
+                ports_list.append(port_object)
+                # switch_node.ports.append(port_object)
+                print("*** PRINT 118***")
+            print("*** PRINT 1182***")
+            print("*** PRINT 1183***")
+            switch_node.ports = ports_list
+            # self.rt.insert(switch_node, commit=True)
+            print("*** PRINT 1184***")
+        print("*** PRINT 119***")
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
+        print("PACKETSSS")
+        print(ev.__dict__)
+        # print(msg.switch)
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         # get Datapath ID to identify OpenFlow switches.
         dpid = datapath.id
+        print("********dpid********"+str(dpid))
         self.mac_to_port.setdefault(dpid, {})
 
         # analyse the received packets using the packet library.
@@ -100,9 +151,92 @@ class OSIRISApp(app_manager.RyuApp):
 
         if eth_pkt.ethertype == ether_types.ETH_TYPE_LLDP:
             self.logger.info("SupLLDP packet in %s %s %s %s %x", dpid, src, dst, in_port, eth_pkt.ethertype)
-            LLDPHost(LLDPPacket.lldp_parse_new(msg.data))
+            lldp_host_obj = LLDPHost(LLDPPacket.lldp_parse_new(msg.data))
+            print("***PACKET****")
+            self.check_and_add_node(lldp_host_obj)
+            self.create_links(datapath, in_port, lldp_host_obj)
+            # print(self.rt.nodes)
             # pprint()
 
+    def check_and_add_node(self, lldp_host_obj):
+        pprint("check_and_add_node***")
+        pprint(lldp_host_obj.__dict__)
+        print("portID:"+lldp_host_obj.port_id)
+        node = self.check_node(lldp_host_obj.system_name)
+        port_name = lldp_host_obj.port_description
+        port_address = lldp_host_obj.port_id
+        port_address_type = "mac"
+        if node is None:
+            print("*** PRINT 1***")
+            port = Port({"name": port_name, "address": {"type": port_address_type, "address": port_address}})
+            print("*** PRINT 12***")
+            self.rt.insert(port, commit=True)
+            print("*** PRINT 13***")
+            pprint(port.__dict__)
+            print("*** PRINT 14***")
+            node = Node({"name": lldp_host_obj.system_name,
+                         "description": lldp_host_obj.system_description})
+            print("*** PRINT 15***")
+            node.ports.append(port)
+            print("*** PRINT 16***")
+            self.rt.insert(node, commit=True)
+            print("*** PRINT 3333***")
+        else:
+            print("*** PRINT 2***")
+            port = self.check_port_in_node(node, port_name)
+            if port is None:
+                port = Port({"name": port_name, "address": {"type": port_address_type, "address": port_address}})
+                node.ports.append(port)
+        print("*** PRINT 4***")
+
+    def create_links(self, datapath, in_port, lldp_host_obj):
+        dpid = datapath.id
+        switch_port = None
+        host_port = None
+        # Find switch Node/ Port Ref
+
+        print("in_port::::::", in_port)
+        print("lldp_host_obj.port_description:::::::::", lldp_host_obj.port_description)
+        for node in self.rt.nodes:
+            if node.name == "switch:"+str(dpid):
+                for port in node.ports:
+                    if port.index == str(in_port):
+                        switch_port = port
+                        break
+            if node.name == lldp_host_obj.system_name:
+                for port in node.ports:
+                    if port.name == lldp_host_obj.port_description:
+                        host_port = port
+                        break
+
+        pprint("======Creating a link between ")
+        pprint(switch_port.__dict__)
+        pprint(host_port.__dict__)
+        if switch_port is not None and host_port is not None:
+            link_name = switch_port.id + ":" + host_port.id
+            link = self.check_link(link_name)
+            if link is None:
+                link = Link({"name": link_name, "directed": False, "endpoints":
+                    [{"rel": "full", "href": switch_port.selfRef}, {"rel": "full", "href": host_port.selfRef}]})
+                self.rt.insert(link, commit=True)
+
+    def check_link(self, link_name):
+        for link in self.rt.links:
+            if link.name == link_name:
+                return link
+        return None
+
+    def check_node(self, node_name):
+        for node in self.rt.nodes:
+            if node.name == node_name:
+                return node
+        return None
+
+    def check_port_in_node(self, node, port_name):
+        for port in node.ports:
+            if port.name == port_name:
+                return port
+        return None
 
 class LLDPHost:
 
@@ -124,13 +258,13 @@ class LLDPHost:
                 pprint("------LLDP_TLV_TTL-----")
             elif tlv.tlv_type == lldp.LLDP_TLV_PORT_DESCRIPTION:
                 pprint("------LLDP_TLV_PORT_DESCRIPTION-----")
-                self.port_description = tlv.tlv_info
+                self.port_description = tlv.tlv_info.decode("utf-8")
             elif tlv.tlv_type == lldp.LLDP_TLV_SYSTEM_NAME:
                 pprint("------LLDP_TLV_SYSTEM_NAME-----")
-                self.system_name = tlv.tlv_info
+                self.system_name = tlv.tlv_info.decode("utf-8")
             elif tlv.tlv_type == lldp.LLDP_TLV_SYSTEM_DESCRIPTION:
                 pprint("------LLDP_TLV_SYSTEM_DESCRIPTION-----")
-                self.system_description = tlv.tlv_info
+                self.system_description = tlv.tlv_info.decode("utf-8")
             elif tlv.tlv_type == lldp.LLDP_TLV_MANAGEMENT_ADDRESS:
                 pprint("------LLDP_TLV_MANAGEMENT_ADDRESS-----")
                 self.parse_management_address(tlv)
@@ -162,15 +296,15 @@ class LLDPHost:
             self.management_addresses.append(self.parse_ipv4_address(tlv_management_address.addr))
         elif tlv_management_address.addr_subtype == 2:
             pprint("---- IPv6 address----")
-            self.management_addresses.append(self.parse_ipv4_address(tlv_management_address.addr))
+            self.management_addresses.append(self.parse_ipv6_address(tlv_management_address.addr))
 # Utilities
     def parse_mac_address(self, hex_string):
-        mac_string = codecs.encode(hex_string, 'hex')
+        mac_string = codecs.encode(hex_string, 'hex').decode('utf-8')
         new_string = ""
         for i in range(0, len(mac_string)):
             if i != 0 and i % 2 == 0:
                 new_string += ':'
-            new_string += mac_string[i]
+            new_string += str(mac_string[i])
         return new_string
 
     def parse_ipv4_address(self, ip_binary_string):
@@ -184,11 +318,11 @@ class LLDPHost:
         return ip_dec_string
 
     def parse_ipv6_address(self, ip_binary_string):
-        ip_hex_string = codecs.encode(ip_binary_string, 'hex')
+        ip_hex_string = codecs.encode(ip_binary_string, 'hex').decode('utf-8')
         pprint(ip_hex_string)
         ipv6_string = ""
         for i in range(0, 8):
-            ipv6_string += ip_hex_string[4 * i:4 * i + 4]
+            ipv6_string += str(ip_hex_string[4 * i:4 * i + 4])
             if i != 7:
                 ipv6_string += ":"
         return ipv6_string
