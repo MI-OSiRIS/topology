918de57621383f3a34a56f4e633cf6a218bd2230
Ezra Kissel <ezkissel@indiana.edu>
2017-03-06 14:38:24 -0500

    Adding RPM packaging support.

---
+++ ryu/config/RPM/centos_postinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ETC=/etc/ryu
+SVDIR=/etc/supervisor
+SHARE=/usr/share/osiris-sdn
+
+if [ ! -d ${ETC} ]; then
+    mkdir -p ${ETC}
+fi
+
+if [ ! -f ${ETC}/osiris.conf ]; then
+    cp ${SHARE}/osiris-sdn-app.conf ${ETC}/osiris.conf
+fi
+
+if grep -q -i "release 6" /etc/redhat-release
+then
+    echo "CentOS 6 scripts unsupported, configure supervisor with template in $SHARE"
+elif grep -q -i "release 7" /etc/redhat-release
+then
+    cp ${SHARE}/supervisor.conf /etc/supervisord.d/osiris-sdn-app.ini
+    cp ${SHARE}/osiris-sdn.service /etc/systemd/system/osiris-sdn.service
+    systemctl daemon-reload
+    systemctl enable osiris-sdn
+    service supervisord restart
+fi
---
+++ ryu/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import setuptools
+setuptools.setup(name='osiris-sdn-app',
+                 description='Topology Discovery and SDN Control Ryu Application',
+                 version="0.1",
+                 data_files=[('/usr/share/osiris-sdn', ['config/RPM/wait_sv_sock',
+                                                        'config/RPM/osiris-sdn.service',
+                                                        'config/supervisor.conf',
+                                                        'config/osiris-sdn-app.conf',
+                                                        'osiris_main.py',])
+                         ],
+                 install_requires=[
+                     'unisrt',
+                     'ryu'
+                 ],
+                 options = {'bdist_rpm':{'post_install' : 'config/RPM/centos_postinstall.sh',
+                                         'post_uninstall' : 'config/RPM/centos_postuninstall.sh'}},
+                 
+             )
