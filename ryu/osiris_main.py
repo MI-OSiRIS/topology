11c21ce32084f58e976ef0a3600e736ce1842bba
grantskipper <grantskipper_2017@depauw.edu>
2017-11-04 16:34:47 -0400

    debugging...

--- ryu/osiris_main.py
+++ ryu/osiris_main.py
@@ -666,7 +666,10 @@ class OSIRISApp(app_manager.RyuApp):
                 print("HOST PORT FOUND - ", host_port)
             else:
                 print("Checking as port number: ", port_number)
-                host_port = self.check_port_in_node_by_port_number(node, port_number)
+                try:
+                    host_port = self.check_port_in_node_by_port_number(node, port_number)
+                except Exception:
+                    return
 
 
             if switch_port is not None and host_port is not None:
